25731ffd7113aabbc8afa72dc7baf75810d084c1
jadhavmrinalini98 <114711523+jadhavmrinalini98@users.noreply.github.com>
2022-11-20 16:27:15 -0500

    initial commit

---
+++ assignment/api/server.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+const userRouter = require('./routes/users')
+const loginRouter = require('./routes/login')
+
+app.use("/user", userRouter)
+app.use("/login", loginRouter)
+
+mongoose.connect("mongodb://127.0.0.1:27017/rest?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false", { family: 4 },
+    () => {
+        console.log("connected to db, starting app")
+        app.listen(3000)
+        console.log("app started")
+    })
+
+
+
